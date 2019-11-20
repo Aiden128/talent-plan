49e8e33b53208058832f6a4f81de039a78c06185
aiden128 <klein128@gmail.com>
2019-11-20 20:42:23 +0800

    version 2: with go routine

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -3,16 +3,95 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
+
+var count = 0
 
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
 func MergeSort(src []int64) {
-	tmp := doSort(src)
+	res := make(chan struct{}, 4)
+	tmp := goDoSemSort(src, res)
 	for i := range tmp {
 		src[i] = tmp[i]
 	}
+}
+
+func goDoSemSort(src []int64, sem chan struct{}) []int64 {
+
+	num := len(src)
+
+	if num < 2 {
+		return src
+	}
+	mid := num / 2
+
+	pool := sync.WaitGroup{}
+	pool.Add(2)
+
+	var (
+		left  = []int64{}
+		right = []int64{}
+	)
+
+	select {
+	case sem <- struct{}{}:
+		go func() {
+			left = goDoSemSort(src[:mid], sem)
+			<-sem
+			pool.Done()
+		}()
+	default:
+		left = goDoSemSort(src[:mid], sem)
+		pool.Done()
+	}
+
+	select {
+	case sem <- struct{}{}:
+		go func() {
+			right = goDoSemSort(src[mid:], sem)
+			<-sem
+			pool.Done()
+		}()
+	default:
+		right = goDoSemSort(src[mid:], sem)
+		pool.Done()
+	}
+
+	pool.Wait()
+	return doMerge(left, right)
+
+}
+
+func goDoSort(src []int64, res chan []int64) {
+
+	num := len(src)
+
+	if len(src) < 2 {
+		res <- src
+		return
+	}
+
+	mid := num / 2
+
+	var (
+		leftChan  = make(chan []int64)
+		rightChan = make(chan []int64)
+	)
+
+	go goDoSort(src[mid:], leftChan)
+	go goDoSort(src[:mid], rightChan)
+
+	l := <-leftChan
+	r := <-rightChan
+
+	close(leftChan)
+	close(rightChan)
+
+	res <- doMerge(l, r)
+	return
 }
 
 func doSort(src []int64) []int64 {
@@ -22,25 +101,19 @@ func doSort(src []int64) []int64 {
 	if num == 1 {
 		return src
 	}
-
 	mid := len(src) / 2
-
 	var (
-		left  = make([]int64, mid)
-		right = make([]int64, num-mid)
+		left  = src[mid:]
+		right = src[:mid]
 	)
-	for i := 0; i < num; i++ {
-		if i < mid {
-			left[i] = src[i]
-		} else {
-			right[i-mid] = src[i]
-		}
-	}
 
 	return doMerge(doSort(left), doSort(right))
 }
 
 func doMerge(left, right []int64) []int64 {
+
+	fmt.Println(count)
+	count++
 
 	total := make([]int64, len(left)+len(right))
 
@@ -54,7 +127,6 @@ func doMerge(left, right []int64) []int64 {
 			total[i] = right[0]
 			right = right[1:]
 		}
-
 		i++
 	}
 
